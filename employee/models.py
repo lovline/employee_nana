fc2a046dc261f930ea41b0628fdf57f3f9b94da8
lovline <1079974085@qq.com>
2018-12-14 00:18:05 +0800

    add empolyee info field to models

--- employee/models.py
+++ employee/models.py
@@ -5,3 +5,15 @@ from django.db import models
 class UserInfo(models.Model):
     username = models.CharField(max_length=32)
     password = models.CharField(max_length=32)
+
+
+class EmployeeInfo(models.Model):
+    name = models.CharField(max_length=32)
+    age = models.IntegerField(max_length=16)
+    gender = models.CharField(max_length=16)
+    phone_number = models.CharField(max_length=16)
+    email = models.EmailField(max_length=64)
+    address = models.CharField(max_length=64)
+    school = models.CharField(max_length=64)
+    department = models.CharField(max_length=64)
+    employment_date = models.DateTimeField(max_length=64, auto_now_add=True)
