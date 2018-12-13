63c4639435ce748a33f59d417bdc874ad20e3393
lovline <1079974085@qq.com>
2018-12-14 00:23:24 +0800

    'max_length' is ignored used with IntegerField

--- employee/models.py
+++ employee/models.py
@@ -9,7 +9,7 @@ class UserInfo(models.Model):
 
 class EmployeeInfo(models.Model):
     name = models.CharField(max_length=32)
-    age = models.IntegerField(max_length=16)
+    age = models.IntegerField()
     gender = models.CharField(max_length=16)
     phone_number = models.CharField(max_length=16)
     email = models.EmailField(max_length=64)
