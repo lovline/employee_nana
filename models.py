efbf804bea8994ba78713d0d864b6ae5abeb8e3a
lovline <1079974085@qq.com>
2018-12-15 21:28:34 +0800

    achieve add and update info.

---
+++ models.py
@@ -0,0 +1,21 @@
+from django.db import models
+# Create your models here.
+
+
+class UserInfo(models.Model):
+    username = models.CharField(max_length=32)
+    password = models.CharField(max_length=32)
+
+
+class EmployeeInfo(models.Model):
+    username = models.CharField(max_length=32)
+    password = models.CharField(max_length=32)
+    age = models.IntegerField()
+    gender = models.CharField(max_length=16)
+    phone_number = models.CharField(max_length=16)
+    email = models.EmailField(max_length=64)
+    interests = models.CharField(max_length=64)
+    address = models.CharField(max_length=64)
+    school = models.CharField(max_length=64)
+    department = models.CharField(max_length=64)
+    employment_date = models.DateTimeField(max_length=64, auto_now_add=True)
